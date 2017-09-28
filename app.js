f07d857c5847c5aadc6c907b24c5e9f818224b86
JC Silverio <silverio@impulsevfx.com>
2017-09-28 13:14:32 -0400

    bug fix, random quote return

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function randomQuotes() {
     randomQuotes();
   } else {
     randomQuoteVal = quotes[randomQuoteIndex];
-    return randomQuoteVal;
+    return quotes[randomQuoteIndex];
   }
 
 }
