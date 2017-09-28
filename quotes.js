32e58898b9fc756ed1351922abf55855e9d8b344
JC Silverio <silverio@impulsevfx.com>
2017-09-28 02:27:15 -0400

    addl quotes addadded

--- quotes.js
+++ quotes.js
@@ -2,7 +2,15 @@
 var quotes = [
 ['If I stop to kick every barking dog, I am not going to get where I’m going.', 'Jackie Joyner-Kersee'],
 ['Optimism is the faith that leads to achievement', 'Helen Keller'],
-['You are the one that possesses the keys to your being. You carry the passport to your own happiness.', 'Diane von Furstenberg']
+['You are the one that possesses the keys to your being. You carry the passport to your own happiness.', 'Diane von Furstenberg'],
+['I was smart enough to go through any door that opened.', 'Joan Rivers'],
+['Cautious, careful people, always casting about to preserve their reputations can never effect a reform', 'Susan B. Anthony'],
+['When the whole world is silent, even one voice becomes powerful.', 'Malala Yousafzai'],
+['Make the most of yourself by fanning the tiny, inner sparks of possibility into flames of achievement.', 'Golda Meir'],
+['Knowing what must be done does away with fear.', 'Rosa Parks'],
+['I didn’t get there by wishing for it or hoping for it, but by working for it', 'Estée Lauder'],
+['Power’s not given to you. You have to take it.', 'Beyoncé Knowles Carter'],
+['The most difficult thing is the decision to act, the rest is merely tenacity.', ' Amelia Earhart']
 ];
 var colors = ['#015d82', '#009fb2', '#d7f4ff', '#f0802b'];
 
